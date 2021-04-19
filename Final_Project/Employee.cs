b225b36bcde0bf5cb69b54752ee86799269afcda
lokeshgrandhe <lokeshgrandhe@gmail.com>
2021-04-19 12:35:31 +0530

    s

---
+++ Final_Project/Employee.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Final_Project
+{
+    public class Employee
+    {
+        
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string Salary { get; set; }
+    }
+}
